3b8f0e063b31f8d4d01ff000c1e1b4a3cd657f85
Chang Jie <changjie@iisii.net>
2012-03-25 23:07:47 +0800

    add article.html

--- app.js
+++ app.js
@@ -41,14 +41,14 @@ app.configure('production', function() {
 
 
 app.get('/', blog.index);
-
+app.get('/article/',blog.article);
 
 app.get('/admin',function(req,res){
 	res.render('admin/index.html',{title:'管理后台'});	
 });
 
 
-app.listen(8080);
+app.listen(9000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 	
